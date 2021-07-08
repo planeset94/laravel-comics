cb771875c2cb840361a3e68820ce9cd2f005a391
planeset94 <planeset94@gmail.com>
2021-07-08 19:33:05 +0200

    Header top: add style

--- laravel-comics/resources/views/layout/app.blade.php
+++ laravel-comics/resources/views/layout/app.blade.php
@@ -24,110 +24,121 @@
     {{-- Site Header --}}
     <header id="site_header">
         <div class="header-top">
-            <p>logo</p>
-            <p>Additional DC Sities<i class="fas fa-caret-down"></i> </p>
+            <div class="container">
+                <p>Dc Power Visa <i class="far fa-registered"></i></p>
+                <p>Additional DC Sities<i class="fas fa-caret-down"></i> </p>
+            </div>
         </div>
         <nav class="menu">
-            <a href="{{ route('characters') }}"
-                class="{{ Route::currentRouteName() === 'characters' ? 'active' : '' }}">Characters</a>
-            <a href="{{ route('comics') }}"
-                class="{{ Route::currentRouteName() === 'comics' ? 'active' : '' }}">Comics</a>
-            <a href="{{ route('movies') }}"
-                class="{{ Route::currentRouteName() === 'movies' ? 'active' : '' }}">Movies</a>
-            <a href="{{ route('tv') }}" class="{{ Route::currentRouteName() === 'tv' ? 'active' : '' }}">Tv</a>
-            <a href="{{ route('games') }}"
-                class="{{ Route::currentRouteName() === 'games' ? 'active' : '' }}">Games</a>
-            <a href="{{ route('collectibles') }}"
-                class="{{ Route::currentRouteName() === 'collectibles' ? 'active' : '' }}">Collectibles</a>
-            <a href="{{ route('videos') }}"
-                class="{{ Route::currentRouteName() === 'videos' ? 'active' : '' }}">Videos</a>
-            <a href="{{ route('fans') }}"
-                class="{{ Route::currentRouteName() === 'fans' ? 'active' : '' }}">Fans</a>
-            <a href="{{ route('news') }}"
-                class="{{ Route::currentRouteName() === 'news' ? 'active' : '' }}">News</a>
-            <a href="{{ route('shop') }}"
-                class="{{ Route::currentRouteName() === 'shop' ? 'active' : '' }}">Shop</a>
+            <div class="container">
+                <a href="{{ route('characters') }}"
+                    class="{{ Route::currentRouteName() === 'characters' ? 'active' : '' }}">Characters</a>
+                <a href="{{ route('comics') }}"
+                    class="{{ Route::currentRouteName() === 'comics' ? 'active' : '' }}">Comics</a>
+                <a href="{{ route('movies') }}"
+                    class="{{ Route::currentRouteName() === 'movies' ? 'active' : '' }}">Movies</a>
+                <a href="{{ route('tv') }}" class="{{ Route::currentRouteName() === 'tv' ? 'active' : '' }}">Tv</a>
+                <a href="{{ route('games') }}"
+                    class="{{ Route::currentRouteName() === 'games' ? 'active' : '' }}">Games</a>
+                <a href="{{ route('collectibles') }}"
+                    class="{{ Route::currentRouteName() === 'collectibles' ? 'active' : '' }}">Collectibles</a>
+                <a href="{{ route('videos') }}"
+                    class="{{ Route::currentRouteName() === 'videos' ? 'active' : '' }}">Videos</a>
+                <a href="{{ route('fans') }}"
+                    class="{{ Route::currentRouteName() === 'fans' ? 'active' : '' }}">Fans</a>
+                <a href="{{ route('news') }}"
+                    class="{{ Route::currentRouteName() === 'news' ? 'active' : '' }}">News</a>
+                <a href="{{ route('shop') }}"
+                    class="{{ Route::currentRouteName() === 'shop' ? 'active' : '' }}">Shop</a>
+            </div>
         </nav>
         <div class="heade-bottom">
-            <div class="current-series"></div>
+            <div class="container">
+                <div class="current-series"></div>
+            </div>
         </div>
 
     </header>
     {{-- /Site Header --}}
 
     <main id="main_content">
-        @yield('content')
+        <div class="container">
+            @yield('content')
+        </div>
     </main>
     <footer id="site_footer">
-        @yield('footer')
-        <section class="top-footer">
-            <div class="link-conteiner">
-                <div class="link-group">
-                    <h3>DC COMICS</h3>
-                    <a href="{{ route('characters') }}">Characters</a>
-                    <a href="{{ route('comics') }}">Comics</a>
-                    <a href="{{ route('movies') }}">Movies</a>
-                    <a href="{{ route('tv') }}">Tv</a>
-                    <a href="{{ route('games') }}">Games</a>
-                    <a href="{{ route('collectibles') }}">Collectibles</a>
-                    <a href="{{ route('videos') }}">Videos</a>
-                    <a href="{{ route('fans') }}">Fans</a>
-                    <a href="{{ route('news') }}">News</a>
-                    <a href="{{ route('shop') }}">Shop</a>
-                </div>
-                <div class="link-group">
-                    <h3>DC</h3>
-                    <a href="#">Term Of Use</a>
-                    <a href="#">Privacy policy (New)</a>
-                    <a href="#">ad Choices</a>
-                    <a href="#">Advertising</a>
-                    <a href="#">Jobs</a>
-                    <a href="#">Subscription</a>
-                    <a href="#">Talent Workshop</a>
-                    <a href="#">CPSC Certificates</a>
-                    <a href="#">Ratings</a>
-                    <a href="#">Shop Help</a>
-                    <a href="#">Contact Us</a>
-                </div>
-                <div class="link-group">
-                    <h3>SITES</h3>
-                    <a href="#">DC</a>
-                    <a href="#">MAD Magazine</a>
-                    <a href="#">DC Kids</a>
-                    <a href="#">DC Universe</a>
-                    <a href="#">DC Power Visa</a>
-                </div>
-                <div class="link-group">
-                    <h3>SHOP</h3>
-                    <a href="#">Shop DC</a>
-                    <a href="#">Shop DC Collectibles</a>
-                </div>
+        <div class="container">
+            @yield('footer')
+            <section class="top-footer">
+                <div class="link-conteiner">
+                    <div class="link-group">
+                        <h3>DC COMICS</h3>
+                        <a href="{{ route('characters') }}">Characters</a>
+                        <a href="{{ route('comics') }}">Comics</a>
+                        <a href="{{ route('movies') }}">Movies</a>
+                        <a href="{{ route('tv') }}">Tv</a>
+                        <a href="{{ route('games') }}">Games</a>
+                        <a href="{{ route('collectibles') }}">Collectibles</a>
+                        <a href="{{ route('videos') }}">Videos</a>
+                        <a href="{{ route('fans') }}">Fans</a>
+                        <a href="{{ route('news') }}">News</a>
+                        <a href="{{ route('shop') }}">Shop</a>
+                    </div>
+                    <div class="link-group">
+                        <h3>DC</h3>
+                        <a href="#">Term Of Use</a>
+                        <a href="#">Privacy policy (New)</a>
+                        <a href="#">ad Choices</a>
+                        <a href="#">Advertising</a>
+                        <a href="#">Jobs</a>
+                        <a href="#">Subscription</a>
+                        <a href="#">Talent Workshop</a>
+                        <a href="#">CPSC Certificates</a>
+                        <a href="#">Ratings</a>
+                        <a href="#">Shop Help</a>
+                        <a href="#">Contact Us</a>
+                    </div>
+                    <div class="link-group">
+                        <h3>SITES</h3>
+                        <a href="#">DC</a>
+                        <a href="#">MAD Magazine</a>
+                        <a href="#">DC Kids</a>
+                        <a href="#">DC Universe</a>
+                        <a href="#">DC Power Visa</a>
+                    </div>
+                    <div class="link-group">
+                        <h3>SHOP</h3>
+                        <a href="#">Shop DC</a>
+                        <a href="#">Shop DC Collectibles</a>
+                    </div>
 
 
-            </div>
+                </div>
 
-            <p>All Site Content TM and @ 2020 DV Entertainment, unless otherwise <a href="#">noted here.</a> All rights
-                reserved.</p>
-            <p><a href="#">Cokies Settings</a></p>
-
+                <p>All Site Content TM and @ 2020 DV Entertainment, unless otherwise <a href="#">noted here.</a> All
+                    rights
+                    reserved.</p>
+                <p><a href="#">Cokies Settings</a></p>
+        </div>
         </section>
 
 
         <section class="bottom-footer">
-            <div class="sign">
-                <h3>Sign-Up Now!</h3>
+            <div class="container">
+                <div class="sign">
+                    <h3>Sign-Up Now!</h3>
+                </div>
+
+                <div class="socials">
+                    <h3>Follow Us</h3>
+                    <span><i class="fab fa-facebook-f"></i></span>
+                    <span><i class="fab fa-twitter"></i></span>
+                    <span><i class="fab fa-youtube"></i></span>
+                    <span><i class="fab fa-pinterest-p"></i></span>
+                    <span><i class="fas fa-map-marker-alt"></i> </span>
+
+                </div>
             </div>
-
-            <div class="socials">
-                <h3>Follow Us</h3>
-                <span><i class="fab fa-facebook-f"></i></span>
-                <span><i class="fab fa-twitter"></i></span>
-                <span><i class="fab fa-youtube"></i></span>
-                <span><i class="fab fa-pinterest-p"></i></span>
-                <span><i class="fas fa-map-marker-alt"></i> </span>
-
-            </div>
-
         </section>
 
 
